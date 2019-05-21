2855a2bb1e7cb220ba2b85e5fd8007729c46f546
fireglow <krosten92@gmail.com>
2019-05-21 11:19:32 +0300

    change front

--- views/manager/_item.php
+++ views/manager/_item.php
@@ -1,4 +1,6 @@
 <div class="thumbnail">
-	<img src="<?=\Yii::$app->imagemanager->getImagePath($model->id, 300, 300)?>" alt="<?=$model->fileName?>">
-	<div class="filename"><?=$model->fileName?></div>
+    <div class="thumbnail-wrap">
+	    <img src="<?=\Yii::$app->imagemanager->getImagePath($model->id, 300, 300)?>" alt="<?=$model->fileName?>">
+	    <div class="filename"><?=$model->fileName?></div>
+    </div>
 </div>
